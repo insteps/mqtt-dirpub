f5a446917145381dd38ba677e2731b1d1516dc9c
V.Krishn <vkrishn4@gmail.com>
2019-01-14 00:16:29 +0530

    bump to v0.7.2

--- sub_client.c
+++ sub_client.c
@@ -1,6 +1,6 @@
 /*
 Copyright (c) 2009-2018 Roger Light <roger@atchoo.org>
-Copyright (c) 2013-2018 V.Krishn <vkrishn@insteps.net>
+Copyright (c) 2013-2019 V.Krishn <vkrishn@insteps.net>
 
 All rights reserved. This program and the accompanying materials
 are made available under the terms of the Eclipse Public License v1.0
